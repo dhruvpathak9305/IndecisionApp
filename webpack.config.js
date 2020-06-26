97d742f1c2c6aee1deaafc78eb30eafc47ae0e36
Dhruv Pathak <dhruvpathak9305@gmail.com>
2020-06-27 02:25:17 +0530

    Import/Export, npm modules, setting up babel with webpack, One component per file, Source Maps with WebPack, WebPack Dev Server, ES6 class property Done

--- webpack.config.js
+++ webpack.config.js
@@ -11,4 +11,17 @@ module.exports = {
     path: path.join(__dirname, "public"),
     filename: "bundle.js",
   },
+  module: {
+    rules: [
+      {
+        loader: "babel-loader",
+        test: /\.js$/,
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  devtool: "cheap-module-eval-source-map",
+  devServer: {
+    contentBase: path.join(__dirname, "public"),
+  },
 };
